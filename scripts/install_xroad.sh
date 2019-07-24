05496e409f60ca077edd6da3b33d629824b0e216
Nero Sule <nero.sule@stormid.com>
2019-07-24 14:59:34 +0100

    Converts CRLF to LF

--- scripts/install_xroad.sh
+++ scripts/install_xroad.sh
@@ -7,43 +7,12 @@ set -euxo pipefail
 echo "Validating script parameters and environment variables ..."
 # ---
 # VARIABLES
-#
-#
 # 
-# [UG-SS] 2.1       Reference Data (User Roles)
-# Registration Officer
-#   xroad-registration-officer
-# System Administrator:
-#   xroad-system-administrator
-# Security Officer:
-#   xroad-security-officer
-#
-#
-# [IG-SS] 2.2       Reference Data
-#
-# [IG-CS] 1.1       X-Road package repository
 xroad_repo='https://artifactory.niis.org/xroad-release-deb'
-# [IG-CS] 1.2       The repository key
 xroad_repo_key='https://artifactory.niis.org/api/gpg/key/public'
-# [IG-CS] 1.3       Account name in the user interface
-#                   Account password
 xroad_username=$xroadUsername
 xroad_password=$xroadPassword
-# [IG-CS] 1.4.0     Ports for inbound connections (from the external network to the central server)
-#                   TCP 4001 service for authentication certificate registration
-#                   TCP 80 distribution of the global configuration
-# [IG-CS] 1.4.1     Port for inbound connections from the management security server
-#                   TCP 4002 management services
-# [IG-CS] 1.5       Port for outbound connections (from the central server to the external network)
-#                   TCP 80 software updates
-# [IG-CS] 1.6       Internal network ports, the user interface port, and management service ports for the management services security server
-#                   TCP 80 HTTP between the central server and the management services' security server
-#                   TCP 4000 user interface
-#                   TCP 4001 HTTPS between the central server and the management services' security server
-#                   TCP 4400 HTTP between central server and management services' security server
-# [IG-CS] 1.7       central server internal IP address(es) and hostname(s)
 # internal_ip=$internalIpAddress
-# [IG-CS] 1.8       central server public IP address, NAT address
 public_ip=$publicIpAddress
 
 # ============================================================
